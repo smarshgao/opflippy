6c5dfcdd6f19a2479f891f7960e6c77aa4aff7ef
smarshgao <71559481+smarshgao@users.noreply.github.com>
2021-06-12 00:49:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,7 @@
 #
 
 # Uncomment a feed source
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
